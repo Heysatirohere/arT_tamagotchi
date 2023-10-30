96b03c6bb86c333a9009d84a56fc70e399c35efe
Heysatirohere <satirog719@gmail.com>
2023-10-29 22:21:30 -0300

    documentação adicionada

---
+++ arT14/Assets/Script/Document.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Document : MonoBehaviour
+{
+    /// <summary>
+    /// https://docs.unity3d.com/ScriptReference/Time-deltaTime.html
+    /// https://www.devmedia.com.br/csharp-switch-case/38214
+    /// https://docs.unity3d.com/ScriptReference/MonoBehaviour.Invoke.html
+    /// https://docs.unity3d.com/ScriptReference/MonoBehaviour.InvokeRepeating.html
+    /// https://docs.unity3d.com/ScriptReference/Mathf.Clamp.html
+    /// https://docs.unity3d.com/ScriptReference/PlayerPrefs.html
+    /// https://docs.unity3d.com/ScriptReference/Object.DontDestroyOnLoad.html
+    /// </summary>
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
